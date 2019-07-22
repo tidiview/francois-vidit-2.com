a1e97daaf11092f33d20d12e5b1e6c33da3ea9b1
tidiview <francois.vidit@laposte.net>
2019-07-22 22:06:28 +0200

    actualisation 20190722_2206

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       options: {
         useMozJpeg: false,
         stripMetadata: true,
-        defaultQuality: 75,
+        defaultQuality: 70,
       }
     },
     `gatsby-transformer-sharp`,
